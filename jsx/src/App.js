657c7813a153fc9c2a011c83c58d80b824b2336c
Seojisoo20191941 <amy0480@naver.com>
2022-06-26 07:08:43 +0900

    add experience code

--- jsx/src/App.js
+++ jsx/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Fragment } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  // 하나의 요소로 감싼다.
+  // return (
+  // <div>
+  //   <h1>멋쟁이사자처럼</h1>
+  //   <p>중앙대학교</p>
+  //   <span>최고!</span>
+  // </div>
 
+  // <puang>
+  //   <h1>멋쟁이사자처럼</h1>
+  //   <p>중앙대학교</p>
+  //   <span>최고!</span>
+  // </puang>
+
+  // <Fragment>
+  //   <h1>멋쟁이사자처럼</h1>
+  //   <p>중앙대학교</p>
+  //   <span>최고!</span>
+  // </Fragment>
+
+  // <>
+  //   <h1>멋쟁이사자처럼</h1>
+  //   <p>중앙대학교</p>
+  //   <span>최고!</span>
+  // </>
+  // );
+
+  // 자바스크립트 표현식
+  // const name = "서지수";
+  // return (
+  //   <>
+  //     <h1>{name}</h1>
+  //   </>
+  // );
+
+  // 조건부 렌더링
+  // const name = "서지쓔";
+  // if (name === "서지수") {
+  //   return <>{name}는 서지수가 맞다</>;
+  // } else {
+  //   return <>{name}는 서지수가 아니다</>;
+  // }
+
+  // const name = "서지수";
+  // return (
+  //   <>
+  //     {name === "서지수" ? (
+  //       <h1>{name}는 서지수가 맞다</h1>
+  //     ) : (
+  //       <h1>{name}는 서지수가 아니다</h1>
+  //     )}
+  //   </>
+  // );
+
+  //   const name = "서즤수";
+  //   return (
+  //     <>
+  //       {name === "서지수" && <h1>{name}는 서지수가 맞다</h1>}
+  //       {name === "서지수" || <h1>{name}는 서지수가 아니다</h1>}
+  //     </>
+  //   );
+  // }
+
+  return <>jsx</>;
+}
 export default App;
