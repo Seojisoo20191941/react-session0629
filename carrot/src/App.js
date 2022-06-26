76ac0ece21999e7e140941083ac4045faf25f56d
Seojisoo20191941 <amy0480@naver.com>
2022-06-27 05:24:48 +0900

    fix:fixed carrot experience

--- carrot/src/App.js
+++ carrot/src/App.js
@@ -1,13 +1,24 @@
 import "./App.css";
 // import { Fragment } from "react";
 import Box from "./Box";
+import styled from "styled-components";
 
 function App() {
+  const StyledApp = styled.div`
+    display: flex;
+    justify-content: center;
+  `;
+  const StyledCard = styled.div`
+    width: 100vh;
+  `;
+
   return (
-    <div>
-      <h3>인기중고</h3>
-      <Box />
-    </div>
+    <StyledApp>
+      <StyledCard>
+        <h3>인기중고</h3>
+        <Box />
+      </StyledCard>
+    </StyledApp>
   );
 }
 
--- carrot/src/Box.js
+++ carrot/src/Box.js
@@ -5,62 +5,30 @@ import "./Box.css";
 
 function Box() {
   const StyledBox = styled.button`
-    border: 1px solid orange;
-    border-radius: 20px;
+    border: 1px solid transparent;
     width: 30vh;
     height: 30vh;
-    font-size: 20px;
+    font-size: 15px;
     background-color: white;
     overflow: hidden;
-    margin: 2vh;
+    margin: 0vh 2vh 2vh 0vh;
+    text-align: left;
   `;
-  const imgList = data.imgs.map((img, index) => (
-    <div key={img.id}>{img.title}</div>
-  ));
-  const titleList = data.titles.map((title, index) => (
-    <div key={title.id}>{title.title}</div>
-  ));
-  const addressList = data.addresses.map((address, index) => (
-    <div key={address.id}>{address.address}</div>
-  ));
-  const pricesList = data.prices.map((price, index) => (
-    <div key={price.id}>{price.price}</div>
-  ));
 
   return (
     <>
-      <StyledBox>
-        {imgList[0]}
-        {titleList[0]}
-        {addressList[0]}
-        {pricesList[0]}
-      </StyledBox>
-      <StyledBox>
-        {titleList[1]}
-        {addressList[1]}
-        {pricesList[1]}
-      </StyledBox>
-      <StyledBox>
-        {titleList[2]}
-        {addressList[2]}
-        {pricesList[2]}
-      </StyledBox>
-      <br />
-      <StyledBox>
-        {titleList[3]}
-        {addressList[3]}
-        {pricesList[3]}
-      </StyledBox>
-      <StyledBox>
-        {titleList[4]}
-        {addressList[4]}
-        {pricesList[4]}
-      </StyledBox>
-      <StyledBox>
-        {titleList[5]}
-        {addressList[5]}
-        {pricesList[5]}
-      </StyledBox>
+      {data.products.map((product) => (
+        <StyledBox key={product.id}>
+          <img src={product.img} alt="#"></img>
+          <text>
+            <p>
+              <b>{product.title}</b>
+            </p>
+            <p className="address">{product.address}</p>
+            <p className="price">{product.price}</p>
+          </text>
+        </StyledBox>
+      ))}
     </>
   );
 }
